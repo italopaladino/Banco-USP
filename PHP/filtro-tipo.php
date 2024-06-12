25e6ba7c9b934b5d2bf8675c7b1681cc926cc111
ItaloIOUSP <italopaladino22@gmail.com>
2024-06-12 18:06:49 -0300

    filtros de consulta

---
+++ PHP/filtro-tipo.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'config.php';
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    // Seleciona o ano e conta quantos registros existem para cada ano
+    $sql = "SELECT tipotrabalho AS tipo, COUNT(*) AS quantidade FROM infogeral GROUP BY tipo";
+    $stm = $pdo->query($sql);
+    $tipostrabalho = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    // Monta a lsta para
+    $filtroHTMLTIPO = "<ul>";
+    foreach ($tipostrabalho as $tipotrabalho) {
+        $tipo = htmlspecialchars($tipotrabalho['tipo']);
+        $quantidade = htmlspecialchars($tipotrabalho['quantidade']);
+        $filtroHTMLTIPO .= "<li><a href='#'>$tipo ($quantidade)</a></li>"; //adiconar link para pesquisa dos anos
+    }
+    $filtroHTMLTIPO .= "</ul>";
+
+    // Retorna a lista HTML
+    echo $filtroHTMLTIPO;
+
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+} finally {
+    if ($pdo) {
+        $pdo = null;
+    }
+}
+?>
