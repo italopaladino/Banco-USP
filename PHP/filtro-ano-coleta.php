3babd5875c14048f0850df5397a46ad25460dfb4
Italo notebook <italopaladino@usp.br>
2024-07-29 17:05:52 -0300

    filtros ano

--- PHP/filtro-ano-coleta.php
+++ PHP/filtro-ano-coleta.php
@@ -4,18 +4,22 @@ require_once 'config.php';
 try {
     $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-
-    // Seleciona o ano e conta quantos registros existem para cada ano
-    $sql = "SELECT EXTRACT(YEAR FROM data2) AS ano, COUNT(*) AS quantidade FROM pontos_coleta GROUP BY ano ORDER BY ano DESC";
+    // Seleciona o ano e conta quantos registros existem para cada ano, considerando apenas um ponto de coleta por trabalhoid
+    $sql = "
+        SELECT EXTRACT(YEAR FROM data2) AS ano, COUNT(DISTINCT trabalhoid) AS quantidade 
+        FROM pontos_coleta 
+        GROUP BY ano 
+        ORDER BY ano DESC
+    ";
     $stm = $pdo->query($sql);
     $anoColetas = $stm->fetchAll(PDO::FETCH_ASSOC);
 
-    // Monta a lsta para
+    // Monta a lista para o filtro
     $filtroHTML = "<ul>";
     foreach ($anoColetas as $anoColeta) {
         $ano = htmlspecialchars($anoColeta['ano']);
         $quantidade = htmlspecialchars($anoColeta['quantidade']);
-        $filtroHTML .= "<li><a href='detalhes.php?ano=".$ano."'>" .$ano. "($quantidade)</a></li>"; //adiconar link para pesquisa dos anos
+        $filtroHTML .= "<li><a href='detalhes.php?ano=".$ano."'>" .$ano. " ($quantidade)</a></li>"; // adicionar link para pesquisa dos anos
     }
     $filtroHTML .= "</ul>";
 
